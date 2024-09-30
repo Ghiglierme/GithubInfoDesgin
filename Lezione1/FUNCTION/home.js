d0ed53d914adc40a6d5251ea1836cbe3612f266c
Ghiglierme <guiribeirolages@gmail.com>
2024-09-30 23:31:28 +0200

    Antes de dormir

--- Lezione1/FUNCTION/home.js
+++ Lezione1/FUNCTION/home.js
@@ -1,35 +1,12 @@
-const images = ["sauna-bagno.jpg", "solar.jpeg", "ssaunana.png"];
-const titles = ["GERADORES DE VAPOR PARA SAUNA", "SAUNA VAPORMAX", "CONFORTO E ECONOMIA"]
-const description = ["TRABALHAMOS COM DIVERSAS OPÇÕES DE GERADORES DE VAPOR PARA SAUNAS.", "TRABALHAMOS COM DIVERSAS OPÇÕES DE GERADORES DE VAPOR PARA SAUNAS.", "COM NOSSOS AQUECEDORES DE ALTA PERFORMANCE!"]
-var current = 0;
+let popup = document.getElementById("popup");
+let description = document.getElementById("popup-description");
 
-function nextImage() {
-    if(current == 2) {
-        current = 0;
-    }
-    else {
-        current = current + 1;
-    }
-    document.getElementById("promo").style.backgroundImage = "url('assets/images/" + images[current] + "')";
-    document.getElementById("desc1").innerHTML = titles[current];
-    document.getElementById("desc2").innerHTML = description[current];
+function openPopup(testo){
+    popup.classList.add("open-popup");
+    description.innerText = testo;
 }
 
-function previousImage() {
-    if(current == 0) {
-        current = 2;
-    }
-    else {
-        current = current - 1;
-    }
-    document.getElementById("promo").style.backgroundImage = "url('assets/images/" + images[current] + "')";
-    document.getElementById("desc1").innerHTML = titles[current];
-    document.getElementById("desc2").innerHTML = description[current];
+function closePopup(){
+    popup.classList.remove("open-popup");
 }
 
-// Chiamato quando viene caricata la pagina
-document.addEventListener("DOMContentLoaded", () => {
-    setInterval(() => {
-        nextImage();
-    }, 3000);
-});
