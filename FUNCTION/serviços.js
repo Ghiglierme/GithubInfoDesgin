04d07d25a04f3b38bfbd0d6837cfe74a7014b1a4
Ghiglierme <guiribeirolages@gmail.com>
2024-10-15 11:18:10 +0200

    tentando arrumar + metade da aula 4

--- FUNCTION/serviços.js
+++
@@ -1,9 +0,0 @@
-function toggle(id) {
-  const p = document.getElementById("dropdown-" + id);
-  if(p.style.display == "block") {
-    p.style.display = "none";
-  }
-  else {
-    p.style.display = "block";
-  }
-}
